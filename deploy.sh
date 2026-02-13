136fe4344e5564cb84dcee5e42471d92a4e998f6
Moshfeka <moshfeka07@gmail.com>
2026-02-13 20:42:23 +0000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 set -ex
-
 echo "======================================"
 echo "🚀 Deploying Docker image on EC2"
 echo "======================================"
 
-# Variables
 REPOSITORY_URI=263751300136.dkr.ecr.us-east-1.amazonaws.com/tailadmin-laravel
 
-# Login to ECR
 echo "🔑 Logging into ECR..."
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin $REPOSITORY_URI
 
-# Pull the latest image
 echo "📥 Pulling Docker image..."
 docker pull $REPOSITORY_URI:$IMAGE_TAG
 
-# Stop old container if exists
 echo "🛑 Stopping old container..."
 docker stop tailadmin-php-fpm || true
 docker rm tailadmin-php-fpm || true
 
-# Start new container
 echo "🚀 Starting new container..."
-docker run -d --name tailadmin-php-fpm -p 9000:9000 --env-file /var/www/tailadmin-laravel/.env \
-$REPOSITORY_URI:$IMAGE_TAG
+docker run -d --name tailadmin-php-fpm \
+  -p 9000:9000 \
+  --env-file /var/www/tailadmin-laravel/.env \
+  -v /var/www/tailadmin-laravel/storage:/var/www/html/storage \
+  $REPOSITORY_URI:$IMAGE_TAG
 
-# Verify
 echo "✅ Deployment complete!"
 docker ps
 
+echo "🔍 Verifying code inside container..."
+docker exec tailadmin-php-fpm cat /var/www/html/test.php
